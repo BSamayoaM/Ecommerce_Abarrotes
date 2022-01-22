46f0f9139dff3d52bc2338e9eeb3729f4568a8b7
BSamayoaM <bsamayoam@toluca.tecnm.mx>
2022-01-22 01:02:39 -0600

    Terminado Modelos Proveedores

--- tienda_abarrotes/proveedores/models.py
+++ tienda_abarrotes/proveedores/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
 class Proveedores(models.Model):
-    pass
+    nombre  =models.CharField(max_length=50)
+    rfc =models.CharField(max_length=50)
+    razon_social =models.CharField(max_length=50)
+    contacto =models.CharField(max_length=50)
+    email =models.EmailField(max_length=50)
+    telefono =models.IntegerField()
+    pais =models.ForeignKey(Paises,on_delete=models.CASCADE)
+    entidad =models.ForeignKey(Entidades,on_delete=models.CASCADE)
+    municipio =models.ForeignKey(Municipios,on_delete=models.CASCADE)
+    direccion =models.CharField(max_length=50)
+    cp =models.CharField(max_length=50)
+    colonia =models.CharField(max_length=50)
     def __str__(self):
-        pass
+        f"{self.nombre} --- {self.razon_social}"
 class Compras(models.Model):
-    pass
+    fecha= models.DateField()
+    subtotal= models.FloatField()
+    iva= models.FloatField()
+    total= models.FloatField()
+    proveedor= models.ForeignKey(Proveedores,on_delete=models.CASCADE)
+    usuarios= models.ForeignKey(Usuarios,on_delete=models.CASCADE)
     def __str__(self):
-        pass
+        f"{self.proveedor} --- {self.fecha} --- {self.total}"
