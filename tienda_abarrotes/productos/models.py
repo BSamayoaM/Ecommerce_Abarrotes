4a687509dbba06074ae6f950aa014f2aa5e38230
BSamayoaM <bsamayoam@toluca.tecnm.mx>
2022-01-22 00:35:44 -0600

    Agregado modelo Productos

--- tienda_abarrotes/productos/models.py
+++ tienda_abarrotes/productos/models.py
@@ -3,9 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Productos(models.Model):
-    pass
+    nombre = models.Charfield(max_length=50)
+    descripcion = models.TextField()
+    existencia = models.IntegerField()
+    precio_compra = models.FloatField()
+    precio_venta = models.FloatField()
+    stock = models.IntegerField()
+    proveedor = models.ForeignKey(Proveedores, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
     def __str__(self):
-        pass
+        f"{self.nombre}"
 class Descuentos(models.Model):
     pass
     def __str__(self):
